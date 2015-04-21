50bf1e579c2ea6d1079ac64472e0dbdd24dc8774
hyunwoo <h.hyunwoo@gmail.com>
2015-04-22 04:02:28 +0900

    메인 수정중.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var route_kmeans = require('./routes/kmeans');
+var route_visual_network = require('./routes/visualization/visual_network');
 var users = require('./routes/users');
 
 
@@ -15,24 +16,23 @@ var busboy = require('connect-busboy'); //middleware for form/file upload
 var path = require('path');     //used for file path
 var fs = require('fs-extra');       //File System - for file manipulation
 
-
-
-
+/*
 var natural = require('natural');
 var wordnet = new natural.WordNet();
 
-wordnet.lookup('node', function(results) {
-    results.forEach(function(result) {
-        /*
-        console.log('------------------------------------');
-        console.log(result.synsetOffset);
-        console.log(result.pos);
-        console.log(result.lemma);
-        console.log(result.synonyms);
-        console.log(result.pos);
-        console.log(result.gloss);*/
+wordnet.lookup('node', function (results) {
+    results.forEach(function (result) {
+
+         console.log('------------------------------------');
+         console.log(result.synsetOffset);
+         console.log(result.pos);
+         console.log(result.lemma);
+         console.log(result.synonyms);
+         console.log(result.pos);
+         console.log(result.gloss);
     });
 });
+*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -43,39 +43,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(busboy());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use('/', routes);
 app.use('/kmeans', route_kmeans);
 app.use('/users', users);
+app.use('/visualization/network', route_visual_network);
 
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 app.listen(3001);
 
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 
-
-
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
---
+++ routes/visualization/visual_network.js
@@ -0,0 +1,39 @@
+/**
+ * Created by hyunwoo on 2015-04-21.
+ */
+/**
+ * Created by Luan on 15. 4. 7..
+ */
+
+var express = require('express');
+var router = express.Router();
+var busboy = require('connect-busboy'); //middleware for form/file upload
+var path = require('path');     //used for file path
+var fs = require('fs-extra');       //File System - for file manipulation
+
+
+
+
+module.exports = router;
+
+
+
+
+router.get('/', function(req, res) {
+
+
+    res.render('visualization/visual_network');
+});
+
+
+router.post('/', function(req, res) {
+    console.log(req.body)
+
+
+    res.render('visualization/visual_network');
+});
+
+
+
+
+
