938ccd298cb405a272f9e057c4ca44b1c35c3e22
Luan <h.hyunwoo@gmail.com>
2015-05-26 10:17:14 +0900

    Add Crawlers

---
+++ crawlers/crawler_weather.js
@@ -0,0 +1,156 @@
+/**
+ * Created by Luan on 15. 5. 26..
+ */
+
+var func = require('../functions/defaultFunctions')
+var APPID = "2ca8a678b40a28ed5f49f8688eae52bb";
+var cities = [
+    {
+        Name: "New York",
+        ID: 5128581
+    }
+    ,
+    {
+        Name: "Nigeria",
+        ID: 2352778
+    }
+    ,
+    {
+        Name: "LosAngeles",
+        ID: 5368361
+    }
+    ,
+    {
+        Name: "Mumai",
+        ID: 1275339
+    }
+    ,
+    {
+        Name: "Singapore",
+        ID: 1880252
+    }
+    ,
+    {
+        Name: "Melbourne",
+        ID: 2158177
+    }
+    ,
+    {
+        Name: "Philippines",
+        ID: 1688216
+    }
+    ,
+    {
+        Name: "New Delhi",
+        ID: 1261481
+    }
+    ,
+    {
+        Name: "London",
+        ID: 2643743
+    }
+    ,
+    {
+        Name: "Toronto",
+        ID: 6167865
+    }
+    ,
+
+]
+
+
+var http = require('http');
+var redis = require('redis');
+var db = redis.createClient(13000, "202.30.24.169");
+
+
+var multi = db.multi();
+multi.select(4);
+for (var i = 0; i < cities.length; i++) {
+    multi.hset("Cities", cities[i].Name, cities[i].ID);
+}
+
+multi.exec(function (err, rep) {
+})
+
+recursiveGetWeather(0);
+
+var ResultPerTime = {};
+
+
+setInterval(function () {
+    recuresiveGetWeather(0);
+
+}, 10800000);
+
+
+function recursiveGetWeather(idx) {
+    var per = Math.floor(func.getDateNoDot() / 30000) * 30000;
+    if (idx >= cities.length) {
+        logging('End Crawler Weather Set');
+        var message = require('util').inspect(ResultPerTime);
+        func.sendEmail("Weather Cralwer Activated", "Result : \n" +message);
+        return;
+    }
+    console.log(cities[idx].Name);
+    http.get("http://api.openweathermap.org/data/2.5/weather?id=" + cities[idx].ID + "&APPID=" + APPID, function (res) {
+        var data = '';
+
+        res.on("data", function (d) {
+            data += d
+        });
+        res.on("end", function () {
+            //console.log(data);
+            //var data = JSON.parse(data);
+            //console.log(data);
+            var input = JSON.parse(data);
+            //console.log(input);
+
+            var result = {
+                lon: input.coord.lon,
+                lat: input.coord.lat,
+                weatherId: input.weather[0].id,
+                weather: input.weather[0].main,
+                desc: input.weather[0].description,
+                temp: Math.floor((input.main.temp * 1 - 273.15)),
+                humidity: input.main.humidity,
+                pressure: input.main.pressure,
+                wind_speed: undefined,
+                wind_deg: undefined,
+                wind_gust: undefined,
+                rain: undefined,
+                snow: undefined,
+            }
+            if (input.rain != undefined) {
+                result.rain = input.rain["3h"];
+            }
+            if (input.snow != undefined) {
+                result.snow = input.snow["3h"];
+            }
+            if (input.wind != undefined) {
+                result.wind_speed = input.wind.speed;
+                result.wind_deg = input.wind.deg;
+                result.wind_gust = input.wind.gust;
+            }
+            console.log(result);
+            var multi = db.multi();
+            multi.select(4);
+            multi.hmset('weather:' + cities[idx].Name + ":" + per , result);
+            multi.exec(function (err, rep) {
+                if (err == null) {
+                    ResultPerTime[cities[idx].Name] = result;
+                } else {
+                    ResultPerTime[cities[idx].Name] = "DB_ERROR";
+                }
+                recursiveGetWeather(idx + 1);
+
+            })
+        })
+    })
+}
+
+
+function logging(s) {
+    var per = Math.floor(func.getDateNoDot() / 30000) * 30000;
+    console.log("[ " + per + " ] : " + s);
+}
--- functions/defaultFunctions.js
+++ functions/defaultFunctions.js
@@ -1,7 +1,71 @@
 /**
- * Created by hyunwoo on 5/12/15.
+ * Created by Luan on 15. 3. 25..
  */
+Date.prototype.format = function(f) {
+    if (!this.valueOf()) return " ";
 
+    var weekName = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+    var d = this;
+
+    return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|ffff|a\/p)/gi, function($1) {
+        switch ($1) {
+            case "yyyy": return d.getFullYear();
+            case "yy": return (d.getFullYear() % 1000).zf(2);
+            case "MM": return (d.getMonth() + 1).zf(2);
+            case "dd": return d.getDate().zf(2);
+            case "E": return weekName[d.getDay()];
+            case "HH": return d.getHours().zf(2);
+            case "hh": return ((h = d.getHours() % 12) ? h : 12).zf(2);
+            case "ffff": return d.getMilliseconds().zf(4);
+            case "mm": return d.getMinutes().zf(2);
+            case "ss": return d.getSeconds().zf(2);
+
+            case "a/p": return d.getHours() < 12 ? "오전" : "오후";
+            default: return $1;
+        }
+    });
+};
+
+String.prototype.string = function(len){var s = '', i = 0; while (i++ < len) { s += this; } return s;};
+String.prototype.zf = function(len){return "0".string(len - this.length) + this;};
+Number.prototype.zf = function(len){return this.toString().zf(len);};
+
+
+
+exports.getDay = function(){
+    var date = new Date();
+    var time = date.format("yyyy.MM.dd");
+    return time;
+}
+exports.getDate = function(){
+    var date = new Date();
+    var time = date.format("yyyy.MM.dd.HH:mm:ss");
+    return time;
+}
+
+exports.getDateNoDot= function(){
+    var date = new Date();
+    var time = date.format("yyyyMMddHHmmss");
+    return time;
+}
+
+
+exports.getDateDetailNoDot= function(){
+    var date = new Date();
+    var time = date.format("yyyyMMddHHmmssffff");
+    return time;
+}
+
+exports.getDateDetail = function(){
+    var date = new Date();
+    var time = date.format("yyyy.MM.dd.HH:mm:ss:ffff");
+    return time;
+}
+
+exports.getTick = function(){
+    var tick = new Date();
+    return tick;
+}
 exports.twitDataToNormalDate = function(date, option){
     var strings = date.split(' ');
     //console.log(" : " + strings);
@@ -9,4 +73,28 @@ exports.twitDataToNormalDate = function(date, option){
     var time = strings[3].split(':')[0];
     var result = strings[5] + " "  + strings[1] + " "  + strings[2] + " "  + strings[0] + " :" + time;
     console.log(result);
+}
+
+
+
+var email = require('emailjs/email');
+var server = email.server.connect({
+    user: "h.hyunwoo@gmail.com",
+    password: "fpdkfvkzm",
+    host: "smtp.gmail.com",
+    ssl: true
+
+});
+
+
+exports.sendEmail = function(title, body) {
+    server.send({
+        text: body,
+        from: "<h.hyunwoo@gmail.com>",
+        to: "<h.hyunwoo@gmail.com>",
+        cc: "",
+        subject: title
+    }, function (err, message) {
+        console.log(err || message);
+    });
 }
