3bce64a749a224784c0e3f205616de06037b99d4
hyunwoo <h.hyunwoo@gmail.com>
2015-09-16 17:23:58 +0900

    cors

--- public/projects/sementicnode/js/sementicnode_viewer.js
+++ public/projects/sementicnode/js/sementicnode_viewer.js
@@ -9,7 +9,7 @@ var svg = d3.select("#canvas").append("svg")
     //.attr("height", height)
 
 //console.log(width,height);
-console.log(document.getElementById("#canvas"));
+console.log(document.getElementById("canvas"));
 console.log(svg);
 var rect = svg.append("rect")
     .attr("width", 100)
@@ -18,3 +18,24 @@ var rect = svg.append("rect")
     .attr("y", 300)
     .style("fill", "#000000")
     .style("pointer-events", "all");
+
+
+httpGet('/apis/twitter/search?q=Donald Trump&onlyDate=true', function(rep){
+
+    console.log(rep);
+    var tweetText;
+    /** TODO
+     * MAKE ERROR RETURN FUNC
+     */
+    if(!rep.success) return;
+
+    for(var i = 0 ; i < rep.tweets.length ; i ++){
+        tweetText += rep.tweets[i].text + "\n";
+    }
+
+    console.log(tweetText);
+    console.log('posting');
+    httpPost('/apis/textanalysis/lda', {'input' : tweetText}, function(rep){
+        console.log(rep);
+    })
+})
