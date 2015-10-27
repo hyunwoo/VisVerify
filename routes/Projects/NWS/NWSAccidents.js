e153828ba8f8c14544e95a7fa986c622f1d8cfd1
hyunwoo <h.hyunwoo@gmail.com>
2015-10-27 17:24:42 +0900

    _

--- routes/Projects/NWS/NWSAccidents.js
+++ routes/Projects/NWS/NWSAccidents.js
@@ -4,7 +4,6 @@
 var express = require('express');
 var router = express.Router();
 
-
 module.exports = router;
 
 var redis = require('redis');
@@ -18,6 +17,8 @@ router.get('/', function (req, res) {
 
 });
 
+/** PostData To Database */
+/*
 var fs = require('fs');
 var posts;
 fs.readFile('./post.tsv', function (err, data) {
@@ -25,41 +26,57 @@ fs.readFile('./post.tsv', function (err, data) {
     posts = data.toString().split('\n');
     savePostToDatabase(0);
 });
+*/
 
 
-function savePostToDatabase(idx) {
-    console.log(" : " + idx);
-    if(idx == posts.length) {
-        console.log(' : over');
-        return;
-    }
-    var keys = posts[idx].split('\t');
-    var input = {
-        '우편번호' : keys[0],
-        '일렬번호' : keys[1],
-        '시/도' :keys[2],
-        '시군구': keys[3],
-        '읍면동':keys[4],
-        '리':keys[5],
-        '도서':keys[6],
-        '건물명':keys[7],
-        '동-시작':keys[8],
-        '동-끝':keys[9],
-        '변경일':keys[10],
-        '주소':keys[11]
-    }
-
-    var total_key = keys[0];
-    var sub_key = keys[0]+':'+keys[1];
+function getPostNumberToAddress(number, func){
     var multi = db.multi();
     multi.select(1);
-    multi.hmset(sub_key,input);
-    multi.sadd(total_key,sub_key);
+    multi.smembers(number);
     multi.exec(function(err,rep){
-        savePostToDatabase(++idx);
-    })
+        if(err != null){
+            func({
+                success : false,
+                ERR : "ERROR",
+            });
+            return;
+        }
+        if(rep[1] === undefined || rep[1] == null || rep[1].length == 0){
+            func({
+                success : false,
+                ERR : "NO DATA",
+            });
+            return;
+        }
+        var multi = db.multi();
+        multi.select(1);
+        var count = 1;//rep[1].length;
+        for(var i = 0 ; i < count ; i ++){
+            multi.hgetall(rep[1][i]);
+        }
 
+        multi.exec(function(err,rep){
+            if(err != null){
+                func({
+                    success : false,
+                    ERR : "ERROR",
+                });
+                return;
+            }
+            var result = [];
+            for(var i = 1 ; i < rep.length ; i ++){
+                result.push(rep[i])
+            }
+            func({
+                success:true,
+                DATA : result
+            })
+        })
+
+    })
 }
+
+
 
 router.get('/PostData', function(req,res){
     var number = req.query.num;
@@ -73,54 +90,11 @@ router.get('/PostData', function(req,res){
         });
         return;
     }
-    var multi = db.multi();
-    multi.select(1);
-    multi.smembers(number);
-    multi.exec(function(err,rep){
-        if(err != null){
-            res.send({
-                success : false,
-                ERR : "DATABASE ERROR",
-            });
-            return;
-        }
-        if(rep[1] === undefined || rep[1] == null || rep[1].length == 0){
-            res.send({
-                success : false,
-                ERR : "NO DATA",
-            });
-            return;
-        }
-        var multi = db.multi();
-        multi.select(1);
-        var count = main == true ? 1 : rep[1].length;
-        for(var i = 0 ; i < count ; i ++){
-            multi.hgetall(rep[1][i]);
-        }
 
-        multi.exec(function(err,rep){
-            console.log(err,rep);
-            if(err != null){
-                res.send({
-                    success : false,
-                    ERR : "DATABASE ERROR",
-                });
-                return;
-            }
-            var result = [];
-            for(var i = 1 ; i < rep.length ; i ++){
-                result.push(rep[i])
-            }
-
-            res.send({
-                success:true,
-                DATA : result
-            })
-        })
-
-
-
+    getPostNumberToAddress(number, function(data){
+        res.send(data);
     })
+
 
 })
 
@@ -128,27 +102,74 @@ router.get('/PostData', function(req,res){
 var countProgress = 0;
 var HighwaydataCount = 0;
 
+var _idx = 0;
+HighWayAddLocate(_idx,HighWayAddLocateFunc);
+function HighWayAddLocateFunc(result){
 
-router.get('/RawData/HighWay', function(req,res){
-    var start = req.query.start;
-    var count = req.query.count;
-    var format = req.query.format;
+    if(result.out == 'continue'){
+        _idx ++;
+        if(_idx % 100 == 0)
+            console.log('\tidx : ' + _idx );
 
-    console.log(count);
-    if (start === undefined || start === "") start = 0;
-    if (count === undefined || count === "") count = 100;
-    if (count > 10000) count = 10000;
-    if (format === undefined || format === "") format = "json";
+        HighWayAddLocate(_idx,HighWayAddLocateFunc);
+    } else if(result.out == 'end'){
+    }
+}
 
+
+function HighWayAddLocate(index, func){
+    getHighWayData(index,index,function(data){
+        if(data.success == false){
+            if(data.Data == 'OVER') {
+                console.log("END");
+                func({
+                    out:'end',
+                    index : index,
+                })
+                return;
+            }
+        }
+        for(var i = 1 ; i < data.data.length ; i ++){
+            var number =data.data[i].numbers ;
+            var id = data.data[i].id;
+            var postNum = number.slice(83,89);
+
+            getPostNumberToAddress(postNum, function(data){
+                var address ;
+                if(data.success) address = data.DATA[0]['주소']
+                else {
+                    address = "NO ADDRESS"
+                    //console.log(postNum , 'has no address data');
+                }
+
+                var multi = db.multi();
+                multi.select(0);
+                multi.hset(id,'address',address);
+                multi.exec(function(err,rep){
+                    func({
+                        out:'continue',
+                        index : index,
+                    })
+                })
+
+            })
+
+        }
+    })
+}
+
+
+function getHighWayData(start,end, func){
     var multi = db.multi();
-    var end = Number(start) + Number(count);
-    console.log("final start : " + start + " , end : " + end);
     multi.select(2);
     multi.hgetall("Text:Header");
     multi.exec(function (err, rep) {
         var dataCount = Number(rep[1].count);
         if (dataCount < start) {
-            res.send("OVER REQUEST INDEX");
+            func({
+                success:false,
+                Data : "OVER",
+            })
             return;
         } else if (end > dataCount) {
             end = dataCount;
@@ -162,29 +183,56 @@ router.get('/RawData/HighWay', function(req,res){
             }
             multi.exec(function (err, rep) {
                 if (err != null) {
-                    res.send("ERROR");
+                    func({
+                        success:false,
+                    })
                     return;
                 }
-                if(format === "csv"){
-                    var result = "<pre>\n";
-                    result += "id\t\tnumbers\t\t\t\t\t\t\t\t\t\t\t\t\t\tbody\n";
-                    for(var i = 0; i < rep.length ; i ++){
-                        if(rep[i].id == undefined) continue;
-                        result += rep[i].id + "\t" + rep[i].numbers + "\t" + rep[i].body + "\n";
-                    }
-                    result += "</pre>";
-                    res.send(result);
-                } else {
-                    var json = JSON.stringify(rep, null, "\t");
-                    var result = "<pre>\n";
-                    result += json;
-                    result += "\n</pre>";
-                    res.send(result);
-                }
+                func({
+                    success:true,
+                    data:rep,
+                })
 
             })
         })
 
+    })
+}
+router.get('/RawData/HighWay', function(req,res){
+    var start = req.query.start;
+    var count = req.query.count;
+    var format = req.query.format;
+
+    console.log(count);
+    if (start === undefined || start === "") start = 0;
+    if (count === undefined || count === "") count = 100;
+    if (count > 10000) count = 10000;
+    if (format === undefined || format === "") format = "json";
+
+    var end = Number(start) + Number(count);
+    getHighWayData(start,end,function(data){
+
+        if(!data.success) {
+            res.send('ERROR');
+            res.end();
+            return;
+        }
+        var rep = data.data;
+        if(format === "csv"){
+            var result = "<pre>\n";
+            for(var i = 0; i < rep.length ; i ++){
+                if(rep[i].id == undefined) continue;
+                result += rep[i].id + "\t" + "\t" + rep[i].body + "\t"+ rep[i].address +"\n";
+            }
+            result += "</pre>";
+            res.send(result);
+        } else {
+            var json = JSON.stringify(rep, null, "\t");
+            var result = "<pre>\n";
+            result += json;
+            result += "\n</pre>";
+            res.send(result);
+        }
     })
 
 });
@@ -246,7 +294,6 @@ router.get('/RawData/All', function (req, res) {
 
 });
 
-
 function makeHighwayData(start, count) {
     var multi = db.multi();
     var end = Number(start) + Number(count - 1);
@@ -307,5 +354,39 @@ function makeHighwayData(start, count) {
         });
     });
 
+
+}
+
+function savePostToDatabase(idx) {
+    console.log(" : " + idx);
+    if(idx == posts.length) {
+        console.log(' : over');
+        return;
+    }
+    var keys = posts[idx].split('\t');
+    var input = {
+        '우편번호' : keys[0],
+        '일렬번호' : keys[1],
+        '시/도' :keys[2],
+        '시군구': keys[3],
+        '읍면동':keys[4],
+        '리':keys[5],
+        '도서':keys[6],
+        '건물명':keys[7],
+        '동-시작':keys[8],
+        '동-끝':keys[9],
+        '변경일':keys[10],
+        '주소':keys[11]
+    }
+
+    var total_key = keys[0];
+    var sub_key = keys[0]+':'+keys[1];
+    var multi = db.multi();
+    multi.select(1);
+    multi.hmset(sub_key,input);
+    multi.sadd(total_key,sub_key);
+    multi.exec(function(err,rep){
+        savePostToDatabase(++idx);
+    })
 
 }
