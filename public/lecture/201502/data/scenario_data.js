9808a96560aa835c13a2dfe603547694a0a6fb92
Luan <h.hyunwoo@gmail.com>
2015-12-27 00:49:43 +0900

    Universe Project List

--- public/lecture/201502/data/scenario_data.js
+++ public/lecture/201502/data/scenario_data.js
@@ -5,7 +5,7 @@
 var scenarios = {};
 
 scenarios['0'] = {
-    name : 'BOBOS',
+    name : '보보스 존',
     img : ['M20'],
     desc : '일과 여가를 즐기며 퇴근 후 시간을 활용하는 젊은 감각의 남성들이 이용하는 Martketplace',
     part : '스크린골프, 서적/도서, PC방'
@@ -13,7 +13,7 @@ scenarios['0'] = {
 }
 
 scenarios['9'] = {
-    name : 'LOEL',
+    name : '인스피리언스존',
     img : ['M20','W20','M40','W40'],
     desc : '남녀노소 불문하고 인테리어에 관심이 많은 이들이 이용하는 Martketplace',
     part : '가정용가구, 조명/전기용품, 철물점'
@@ -21,7 +21,7 @@ scenarios['9'] = {
 }
 
 scenarios['7'] = {
-    name : 'Mr.Routine',
+    name : '루틴 존',
     img : ['M40'],
     desc : '하루하루 최선을 다하며 소소한 행복을 추구하는 가장 standard한 직장인들의 Marketplace',
     part : '일반식당, 활어회, 스크린골프'
@@ -29,7 +29,7 @@ scenarios['7'] = {
 }
 
 scenarios['2'] = {
-    name : 'Nightlife',
+    name : '올뺴미 존',
     img : ['M20','W20'],
     desc : '밤의 문화를 즐기는 20대 남녀의 Martketplace',
     part : '클럽주점, 모텔, pc방'
@@ -38,7 +38,7 @@ scenarios['2'] = {
 
 
 scenarios['6'] = {
-    name : 'RUDY',
+    name : '포미 존',
     img : ['W20'],
     desc : '외모에 관심이 많고, 자투리 시간을 활용하는 젊은 감각의 여성들이 이용하는 Martketplace',
     part : '피부과, 성형외과, 미용실'
@@ -46,7 +46,7 @@ scenarios['6'] = {
 }
 
 scenarios['11'] = {
-    name : '2080',
+    name : '2080 존',
     img : ['M20','W20','M60','W60'],
     desc : '남녀노소 누구나 쉽게 이용하는단지 내 중심 Martketplace',
     part : '한정식, 베이커리, 아이스크림'
@@ -54,7 +54,7 @@ scenarios['11'] = {
 }
 
 scenarios['10'] = {
-    name : 'Bravolife',
+    name : '브라보라이프존',
     img : ['M40','W40','M60','W60'],
     desc : '장년, 노년층들이 시시각각 이용하는 병원/식당가 Martketplace',
     part : '해장국, 보리밥, 내과'
@@ -63,7 +63,7 @@ scenarios['10'] = {
 
 
 scenarios['14'] = {
-    name : 'Graceman',
+    name : '슬로비 존',
     img : ['M40','M60'],
     desc : '스포츠레저에 취미가 있는 이들이 이용하는 등산로 주변 Marketplace',
     part : '스포츠/등산, 한정식, 닭갈비'
@@ -72,7 +72,7 @@ scenarios['14'] = {
 
 
 scenarios['12'] = {
-    name : 'Silverlife',
+    name : '실버 존',
     img : ['M60','W60'],
     desc : '소소한 외출을 즐기는 시니어들이 이용하는 단지내 작은 Martketplace',
     part : '일반식당, 신경외과, 슈퍼마켓'
